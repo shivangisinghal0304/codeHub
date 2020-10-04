896ac068f1d29e68231bab17d3347f682a53c5f9
shivangisinghal0304 <63302831+shivangisinghal0304@users.noreply.github.com>
2020-10-04 12:12:53 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class power {
+	
+	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n =  sc.nextInt();
+		System.out.println("Enter the powrer");
+		int a= sc.nextInt();
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
